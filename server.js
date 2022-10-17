6705aae437fbcb8f050f00b348158506af4e43a5
priya2201 <priyamurpani99@gmail.com>
2022-10-17 17:45:35 +0530

    closure

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const bodyParser=require("body-parser");
 
 const mysqlConnection=require("./connection");
 const peopleRoutes=require("./routes/people");
+const EducationRoutes=require("./routes/education")
 var app=express();
 app.use(bodyParser.json());
 app.use("/people",peopleRoutes);
+app.use("/education",EducationRoutes);
 app.listen(3000);
